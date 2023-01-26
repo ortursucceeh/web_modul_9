ffd1ba60e32928beb95b1191098b0cbbabb55184
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-01-26 19:53:28 +0200

    add get_authors and load_in_json functions

--- beautiful_soup/main.py
+++ beautiful_soup/main.py
@@ -1,6 +1,10 @@
+import json
+from typing import List, Set
+from datetime import datetime
+
 import requests
 from bs4 import BeautifulSoup
-from typing import List
+
 
 base_url = "http://quotes.toscrape.com"
 
@@ -16,14 +20,17 @@ def get_quotes() -> List[dict]:
 
     for author, text, tags in zip(quote_author, quote_text, quote_tags):
         quote = {}
-        quote['author'] = author.string
-        quote['quote'] = text.string
+
         quote['tags'] = tags['content'].split(',')
+        quote['author'] = author.text
+        print(author.text)
+        quote['quote'] = text.text
+
         quotes.append(quote)
     
     return quotes
 
-def get_url_authors():
+def get_url_authors() -> Set[str]:
     response = requests.get(base_url)
     soup = BeautifulSoup(response.text, 'html.parser')
     author_links = soup.select('div[class=quote] span a')
@@ -32,14 +39,42 @@ def get_url_authors():
     for link in author_links:
         urls.append(base_url + link['href'])
 
-    return urls
+    return set(urls)
 
 
-def get_authors():
-    pass
+def get_authors(urls: Set[str]):
+    authors = []
+    for url in urls:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+
+        fullname = soup.select('h3[class=author-title]')
+        born_date = soup.select('span[class=author-born-date]')[0].text
+        born_location = soup.select('span[class=author-born-location]')[0].text
+        description = soup.select('div[class=author-description]')[0].text
+        author = {}
+        author['fullname'] = fullname
+        author['born_date'] = datetime.strptime(born_date, "%B %d, %Y").isoformat()
+        author['born_location'] = born_location
+        author['description'] = description.strip()
+
+        authors.append(author)
+
+    return authors
+
+def load_in_json(filename: str, data: List[dict]):
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(data, file)
+
+
+
+    
 
 if __name__ == '__main__':
     quotes = get_quotes()
     urls = get_url_authors()
-    print(urls)
-    
+    authors = get_authors(urls)
+    load_in_json('authors.json', authors)
+    load_in_json('quotes.json', quotes)
+    # print(quotes)
